66879ee6bbefedfa18ddc968dadd8dc88bf0284c
f-prouny <f.prouny76@gmail.com>
2022-10-17 14:32:24 +0200

    creation du controlleur

--- User/src/Controller/UserController.php
+++ User/src/Controller/UserController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class UserController extends 
 AbstractController
@@ -17,6 +23,14 @@ AbstractController
         return $this->render('user/index.html.twig' );
     }
     /**
-     * @Route
+     * @Route("/inscription",name="registration")
      */
+    public function inscription(ManagerRegistry $doctrine,Request $resq, UserPasswordEncoderInterface $encoder, SluggerInterface $slugger){
+        $user = new User();
+        $form = $this->createForm(UserType::class,$user);
+
+        return $this->render("user/add.html.twig",[
+            "formulaire"=>$form->createView()
+        ]);
+    }
 }
