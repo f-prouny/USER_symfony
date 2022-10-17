1346005f664d89f7bfbe2517981c6f2aadd6fd2e
f-prouny <f.prouny76@gmail.com>
2022-10-17 15:09:30 +0200

    ajout de l'avatar

--- User/src/Controller/UserController.php
+++ User/src/Controller/UserController.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use App\Form\UserType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -28,6 +29,37 @@ AbstractController
     public function inscription(ManagerRegistry $doctrine,Request $resq, UserPasswordEncoderInterface $encoder, SluggerInterface $slugger){
         $user = new User();
         $form = $this->createForm(UserType::class,$user);
+
+        $form->handleRequest($resq);
+        if($form->isSubmitted()&& $form->isValid()){
+            $hash = $encoder($user,$user->getPassword
+            ());
+        $user->setPassword($hash);
+
+        // ajout photo de profil
+        /** @var UploadedFile $imgFile
+        
+         */
+        $imgFile = $form->get('avatar')->getData();
+        if($imgFile){
+            $originalFileName = pathinfo
+            ($imgFile->getClientOriginalName(),
+            PATHINFO_FILENAME);
+            $safeFilmName = $slugger->slug($originalFileName);
+            $newFileName= $safeFilmName."-".uniqid().".".
+            $imgFile->guessExtension();
+
+            try {
+              $imgFile->move($this->getParameter('avatar'),
+              $newFileName);  
+            } catch (FileException $th) {
+                $th->getMessage();
+
+            }
+            $user->setAvatar($newFileName);
+        }
+        $objectManager = $doctrine->getManager();
+        }
 
         return $this->render("user/add.html.twig",[
             "formulaire"=>$form->createView()
